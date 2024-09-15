0760a5681f55cac1108a75230821de6b0de903aa
mnani87 <mihir.naniwadekar@pm.me>
2024-09-16 02:40:21 +0530

    Update clarity.py

--- clarity.py
+++ clarity.py
@@ -1,3 +1,20 @@
+# Clarity Notes - A Distraction-Free Text Editor
+# Copyright (C) 2024 MCN
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# See <https://www.gnu.org/licenses/>.
+
+
+
 import sys
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QTextEdit, QPushButton, QVBoxLayout, QWidget, QHBoxLayout,
